5af31872ada9d5689d54839039ecd3aef722a582
jess <tjessica10@gmail.com>
2024-02-04 18:45:24 -0500

    Indicate source of exercises

--- sql-exercises/1-adv-1-data-types.sql
+++ sql-exercises/1-adv-1-data-types.sql
@@ -1,4 +1,7 @@
 -- Convert the funding_total_usd and founded_at_clean columns in the tutorial.crunchbase_companies_clean_date table to strings (varchar format) using a different formatting function for each one.
+
+-- Source: https://mode.com/sql-tutorial/sql-data-types
+
 SELECT CAST(funding_total_usd AS varchar) AS funding_total_usd_string,
        founded_at_clean::varchar AS founded_at_string
   FROM tutorial.crunchbase_companies_clean_date
